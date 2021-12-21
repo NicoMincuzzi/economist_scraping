6d452bb9910e346c2aaf7f01d4f5b5762be7281e
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-12-21 23:39:58 +0100

    [feat]: Define Article mongodb schema

---
+++ src/repository/article.schema.ts
@@ -0,0 +1,20 @@
+import {Document, model, Schema} from "mongoose";
+
+interface IArticle extends Document {
+    articleId: string;
+    title: string;
+    subtitle: string;
+    body?: string;
+    lastUpdated?: Date;
+}
+
+const ArticleSchema = new Schema<IArticle>({
+    articleId: {type: String, required: true},
+    body: String,
+    lastUpdated: {default: new Date(), type: Date},
+    subtitle: {type: String, required: true},
+    title: {type: String, required: true},
+});
+
+const Article = model<IArticle>("Article", ArticleSchema);
+export default Article;
